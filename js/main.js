f7fabe29d3016704d8a2131fec776d267e0e2256
Antonio Valdivia <antoniovaldivia@gmail.com>
2020-03-27 09:39:35 -0400

    Animation added

--- js/main.js
+++ js/main.js
@@ -1,24 +1,32 @@
+// Function to get dice's random value
 
-let setRandomDiceValue = () => {
-    let DiceValue = Math.floor(Math.random() * 6) + 1; 
-    if (DiceValue == 0 ) { 
-    DiceValue = 1;
-    }
+
+// Function to "roll" the dice by calling the previous function 3 times
+let i = 0
+
+let setRandomDiceValue = () => {  // Handler
+	// Do the dice roll here
+	 let DiceValue = Math.floor(Math.random() * 6) + 1; 
+    // Update HTML content
     document.querySelector("#diceimg").setAttribute('src', `/img/dice${DiceValue}.svg`);
     document.querySelector("#rolledText").innerHTML = `You rolled ${DiceValue}`;
-    // document.querySelector(`body`).setAttribute('title', `Random Dice Roll - ${DiceValue}`);
+     // Change the document's title 
     document.title = `Random Dice Roll - ${DiceValue}`; 
-    console.log(DiceValue);
-};
+	// Now check if we should set another timeout, or just reset the clock for next time
+	if (i < 3) {
+		setTimeout(setRandomDiceValue, 100)
+		i++
+	} else {
+		i = 0
+	}
+}
 
-document.querySelector("#rollBtn").addEventListener('click', setRandomDiceValue);
+document.querySelector("#rollBtn").addEventListener('click', setRandomDiceValue)  
 
-
-/* document.querySelector("body").addEventListener('mousemove', setRandomDiceValue);
- */
-/* 
+/* Steps: 
 1. Get the random value.
-2. Change html values.
+2. Change HTML values.
 3. Set the listener on click button.
 4. Deal with the zero.
+5. Roll the dice?
  */
